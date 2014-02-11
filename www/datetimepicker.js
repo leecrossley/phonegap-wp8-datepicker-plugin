57faf51d49df688c62b5ddd1be67ce25b93a762e
Lee Crossley <leee@hotmail.co.uk>
2014-02-11 13:03:58 +0000

    Clean js

--- www/datetimepicker.js
+++ www/datetimepicker.js
@@ -1,58 +1,16 @@
 
-    var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
-    var pickerexport = {};
+var DateTimePicker = function () {
+    this.name = "DateTimePicker";
+};
 
-    /**
-    * Open DateTime picker to select a date
-    *
-    * @param {Function} successCallback
-    * @param {Function} errorCallback
-    * @param {Object} options - additional options: 'value' - initial value for date
-    */
-    pickerexport.selectDate = function (successCallback, errorCallback, options) {
-        if (successCallback && (typeof successCallback !== "function")) {
-            console.log("DateTimePicker Error: successCallback is not a function");
-            return;
-        }
+DateTimePicker.prototype.selectDate = function (onSuccess, onError, value) {
+    exec(onSuccess.bind(null), onError, "DateTimePicker", "selectDate", value);
+};
 
-        if (errorCallback && (typeof errorCallback !== "function")) {
-            console.log("DateTimePicker Error: errorCallback is not a function");
-            return;
-        }
+DateTimePicker.prototype.selectTime = function (onSuccess, onError, value) {
+    exec(onSuccess.bind(null), onError, "DateTimePicker", "selectTime", value);
+};
 
-        exec(function (res) {
-
-            successCallback(new Date(parseFloat(res)));
-
-        }, errorCallback, "DateTimePicker", "selectDate", options.value);
-    };
-
-    /**
-    * Open DateTime picker to select a time
-    *
-    * @param {Function} successCallback
-    * @param {Function} errorCallback
-    * @param {Object} options - additional options: 'value' - initial value for time
-    */
-    pickerexport.selectTime = function (successCallback, errorCallback, options) {
-        if (successCallback && (typeof successCallback !== "function")) {
-            console.log("DateTimePicker Error: successCallback is not a function");
-            return;
-        }
-
-        if (errorCallback && (typeof errorCallback !== "function")) {
-            console.log("DateTimePicker Error: errorCallback is not a function");
-            return;
-        }
-
-        exec(function (res) {
-
-         successCallback(new Date(parseFloat(res)));
-
-        }, errorCallback, "DateTimePicker", "selectTime", options.value);
-    };
-
-
-    module.exports = pickerexport;
-
+module.exports = new DateTimePicker();
